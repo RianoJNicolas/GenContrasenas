634e0f55413ed1d7600dea096ca4d5cd6ef6a827
rianojnicolas <rianojnicolas@gmail.com>
2021-09-25 23:33:32 -0500

    Actualizando listas de caracteres

--- generador_contrasenas.py
+++ generador_contrasenas.py
@@ -1,9 +1,9 @@
 import random
 
 def generar_contrasena():
-    mayus = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q']
-    minus = ['a','b','c','d','e','f','g','h']
-    simbolos = ['!','#','$','%','&','/','(',')']
+    mayus = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z'],
+    minus = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'],
+    simbolos = ['¡','!','#','$','%','&','/','(',')','=','*','[',']','¿','?','{','}','<','>','@','|','°'],
     numeros = ['1','2','3','4','5','6','7','8','9']
 
     caracteres = mayus + minus + simbolos + numeros
